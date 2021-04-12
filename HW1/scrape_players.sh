74ce7313daa4a21586c60ee175e3b9791ca12cee
milraderez <milraderez@campus.technion.ac.il>
2021-04-12 14:42:21 +0300

    Fixed

--- HW1/scrape_players.sh
+++ HW1/scrape_players.sh
@@ -2,18 +2,25 @@
 chmod +x ./scrape_players.sh
 wget https://premierleague.com/players
 #taking only the relevant players
-cat players | grep -P -o "/players/[0-9]+/[a-z A-Z -]+/overview" players >> mid
+#rel = relevant players 
+cat players | grep -P -o "/players/[0-9]+/[a-z A-Z -]+/overview" players >> rel
 rm players
-sed -i 's/\/players/https:\/\/www.\premierleague\.com\/players/' mid
+sed -i 's/\/players/https:\/\/www.\premierleague\.com\/players/' rel
 #adding the word midfielder, spaces and commas to the file
-awk 'BEGIN{FS=OFS=", "}{$2 ="Midfielder, "}1' mid >> temp_results.csv
+#tres.csv = temp_results.csv
+awk 'BEGIN{FS=OFS=", "}{$2 ="Midfielder, "}1' rel >> tres.csv
 #counting the number of "midfielder" appearences for each player
-while read -r line; do wget -O- $line | grep -P -i -c 'Midfielder' >> temp_count.csv;done <mid
+#tco.csv = temp_count.csv
+while read -r line;
+do wget -O- $line | grep -P -i -c 'Midfielder' >> tco.csv;done < rel   
 #adding the numbers from temp count in line
-awk 'FNR==NR{a[NR] = $1;next}{$3 = a[FNR]}1' temp_count.csv temp_results.csv >> temp_results.csv
-rm temp_count.csv
-echo "Total Players: $(cat mid |wc -l) " > Results.csv
-rm mid
+awk 'FNR==NR{a[NR] = $1;next}{$3 = a[FNR]}1' tco.csv tres.csv >> tres.csv
+rm tco.csv
+echo "Total Players: $(cat rel |wc -l) " > Results.csv
+rm rel
 #picking only the "real" midfielders for the final file
-cat temp_results.csv | grep -P -o "https://www.premierleague.com/players/[0-9]+/[a-z A-Z -]+/overview, Midfielder, [1-9]+" temp_results.csv >> Results.csv
-rm temp_results.csv
+address="https://www.premierleague.com/players"
+look_for="[0-9]+/[a-z A-Z -]+/overview, Midfielder, [1-9]+"
+cat tres.csv |
+ grep -P -o "$address/$look_for" tres.csv >> Results.csv
+rm tres.csv
