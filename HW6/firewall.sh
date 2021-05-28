617564a893db28016bf011d2fda215838e154a7c
milraderez <82085611+milraderez@users.noreply.github.com>
2021-05-28 09:31:58 +0300

    with extra commas deduction

--- HW6/firewall.sh
+++ HW6/firewall.sh
@@ -6,11 +6,12 @@ passed_all=""
 
 	while read rule
 	do
-		#eliminate spaces,empty lines, and comment lines
-		rule_cut=`echo "$rule" | sed 's/ //g' | sed '/^$/d' | grep -o '^[^#]*'`
-		if [[ -n $rule_cut ]]; then
+		#eliminate spaces,empty lines,comment lines, and extra commas
+		rule_cut=`echo "$rule" |sed 's/,\{2,\}//g' | sed 's/ //g' | sed '/^$/d'`
+		only_rule=`echo "$rule_cut"| sed 's/,\{2,\}/,/g' | grep -o '^[^#]*'`
+		if [[ -n $only_rule ]]; then
 			#split the rule to different filters
-			readarray -d , -t filter_type <<< "$rule_cut"
+			readarray -d , -t filter_type <<< "$only_rule"
 			passed_1=`echo "$packets" | ./firewall.exe "${filter_type[0]}"`
 			passed_2=`echo "${passed_1}" | ./firewall.exe "${filter_type[1]}"`
 			passed_3=`echo "${passed_2}" | ./firewall.exe "${filter_type[2]}"`
