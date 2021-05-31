d8d577f47b2b515f0c595e4e74f885e6bd102b69
milraderez <milraderez@campus.technion.ac.il>
2021-05-31 21:02:19 +0300

    final

--- HW6/firewall.sh
+++ HW6/firewall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 chmod +x ./firewall.sh
-#to read stdin value before loop changes cat.
+#read stdin value before loop changes cat + remove tabs.
 packets=`cat | tr -d '\t'`
 passed_all=""
 
 	while read rule
 	do
-		#eliminate spaces,empty lines, and comment lines
+		#eliminate spaces,empty lines,comment lines, and commas
 		rule_cut=`echo "$rule" | sed 's/ //g' |sed 's/,\{2,\}/,/g' |sed '/^$/d'`
-		only_rule=`echo "$rule_cut" | grep -o '^[^#]*'`
+		only_rule=`echo "$rule_cut" | grep -o '^[^#]*' | sed 's/^,\{1,\}//'`
 		if [[ -n $only_rule ]]; then
 			#split the rule to different filters
 			readarray -d , -t filter_type <<< "$only_rule"
@@ -20,7 +20,7 @@ passed_all=""
 			passed_all+=`echo "${passed_4}" |sed 's/src-ip/\nsrc-ip/'`
 		fi
 	done < "$1"
-#-e to treat the \n, sed for removing emtpy lines and spaces, and sort + unique
+# -e to treat the \n, sed for removing emtpy lines and spaces, and sort + unique
 echo -e "$passed_all" | sed '/^$/d' | sed 's/ //g' | sort -u
 
 
